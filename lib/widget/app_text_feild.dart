5782f99742857337af9fefa90cf0a4faadc9d6b8
Nipuana <nipuanarimal@gmail.com>
2025-12-05 00:58:22 +0545

    changed design of standart text feild

---
+++ lib/widget/app_text_feild.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppTextFeild extends StatelessWidget {
+  final String hint;
+  final IconData prefixIcon;
+  final IconData? suffixIcon;
+  final bool obscure;
+  final VoidCallback? onSuffixTap;
+
+  const AppTextFeild({super.key, 
+    required this.hint,
+    required this.prefixIcon,
+    this.suffixIcon,
+    this.obscure = false,
+    this.onSuffixTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: obscure,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.grey.shade100,
+        hintText: hint,
+        prefixIcon: Icon(prefixIcon),
+
+        suffixIcon: suffixIcon != null
+            ? GestureDetector(
+                onTap: onSuffixTap,
+                child: Icon(suffixIcon),
+              )
+            : null,
+
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
