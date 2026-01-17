160b5e8434fd638c2eebc2c462edc6b71c54fe25
Nipuana <nipuanarimal@gmail.com>
2026-01-17 21:52:48 +0545

    added and changed logic in auth datasource

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -1,6 +1,15 @@
 import 'package:weplay_music_streaming/features/auth/data/models/auth_hive_model.dart';
 
-abstract interface class IAuthDatasource{
+abstract interface class IAuthLocalDatasource{
+  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel?> login(String email, String password);
+  Future<AuthHiveModel?> getCurrentUser();
+  Future<bool> logout();
+
+}
+
+
+abstract interface class IAuthRemoteDatasource{
   Future<bool> register(AuthHiveModel model);
   Future<AuthHiveModel?> login(String email, String password);
   Future<AuthHiveModel?> getCurrentUser();
